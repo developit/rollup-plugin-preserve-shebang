110235c80cac1bfd7e677ac5e7ec50aba80823d5
Jason Miller <jason@developit.ca>
2017-12-11 19:47:11 -0500

    Initial commit! 🚀

---
+++ index.js
@@ -0,0 +1,36 @@
+import MagicString from 'magic-string';
+import fs from 'fs';
+
+export default function shebangPlugin({ shebang, entry }={}) {
+	function processEntry(entry) {
+		if (entry) {
+			let contents = fs.readFileSync(entry, 'utf8');
+			let matches = contents.match(/^\s*(#!.*)/);
+			shebang = matches && matches[1] || false;
+		}
+	}
+	processEntry(entry);
+
+	return {
+		options(options) {
+			if (!entry) {
+				entry = options.input;
+				processEntry(entry);
+			}
+			return options;
+		},
+		transformBundle(code, { format, sourceMap }) {
+			if (!shebang) return;
+
+			let str = new MagicString(code);
+			str.prepend(shebang+'\n');
+			let result = {
+				code: str.toString()
+			};
+			if (sourceMap!==false) {
+				result.map = str.generateMap({ hires: true });
+			}
+			return result;
+		}
+	};
+}
