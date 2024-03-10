c5aef19635a06ce7ec9b2755e47d1f5eb71417fa
Michael Wuergler <wuergler@gmail.com>
2024-03-10 10:29:29 +0100

    refactor: minor cleanup

--- __tests__/test-utils.mjs
+++ __tests__/test-utils.mjs
@@ -1,19 +1,16 @@
+import { fileURLToPath } from 'url';
 import { rollup } from 'rollup';
 import path from 'path';
-import shebangPlugin from '../index'; // Adjust the import path as necessary
+
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { fileURLToPath } from 'url';
+
+import shebangPlugin from '../index';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const fixturesDir = path.join(__dirname, 'fixtures');
-
-async function getGeneratedContent(bundle, format = 'cjs') {
-	const { output } = await bundle.generate({ format });
-	return output[0].code;
-}
 
 async function getRollUpBundle(inputFilePath, shebangPluginOptions = {}) {
 	const bundle = await rollup({
@@ -26,6 +23,11 @@ async function getRollUpBundle(inputFilePath, shebangPluginOptions = {}) {
 
 function getFixtureFilePath(filename) {
 	return path.join(fixturesDir, filename);
+}
+
+async function getGeneratedContent(bundle, format = 'cjs') {
+	const { output } = await bundle.generate({ format });
+	return output[0].code;
 }
 
 export { getRollUpBundle, getGeneratedContent, getFixtureFilePath };
